5751cfafbc8564034a084811649ac0b5b77ea56a
Gelldur <gelldur@o2.pl>
2012-01-12 22:01:28 +0100

    D

--- ksoap2-base/src/main/java/org/ksoap2/serialization/SoapObject.java
+++ ksoap2-base/src/main/java/org/ksoap2/serialization/SoapObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003,2004, Stefan Haustein, Oberhausen, Rhld., Germany
+ * opyright (c) 2003,2004, Stefan Haustein, Oberhausen, Rhld., Germany
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
